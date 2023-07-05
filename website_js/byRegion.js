fa49ee512174d61c95f5b249009417ce7f8e16de
matthewc <122582438+SolSolstice@users.noreply.github.com>
2023-07-04 21:18:50 -0400

    Add files via upload
    days work - started by Price page ~ figuring out how to properly center text over image and preserve proper image dimensions

--- website_js/byRegion.js
+++ website_js/byRegion.js
@@ -1,31 +1,10 @@
 
 
-// read in the file 
-// split by the new line and split by the comma
-// make it into object
-
-
-/*
-
-const fs = require('fs')
-var parse = require('csv-parse')
-fs.readFile(inputPath, function (err, fileData) {
-  parse(fileData, {columns: false, trim: true}, function(err, rows) {
-    // Your CSV data is in an array of arrys passed to this callback as rows.
-  })
-})
-
-
-
-*/
-
-
-
-
+// using a promise to read in our csv
 d3.csv("../static/wineDF.csv").then(function (data){
     var wines = data;
-    var button = d3.select("#button");
-    var form = d3.select("#form");
+    var button = d3.select("#region-button");
+    var form = d3.select("#region-form");
     button.on("click",runEnter);
     form.on("submit",runEnter);   
 
@@ -35,36 +14,60 @@ d3.csv("../static/wineDF.csv").then(function (data){
 function runEnter() {
 
 
-    /*  This line of code selects the <tbody> from the html and clears it.
-    If this is not used, then the results would appear on top of the previous result.
+    /*  This selects the <tbody> from the html and clears it.
+   this prevents results from appearing on top of the previous result.
     */
     d3.select("tbody").html("")
  
+    // stops the website from immediately clearing the output
     event.preventDefault(); 
-    //This code will get the user's input from what the user will type in the html <input> since we assigned it the "user-input" id.
-    //It will get the value and store it in our inputValue variable
+
+
+    //This takes user's input from the html <input>
+    // since we assigned it the "user-input" id.
+    //It will get the value and store it in our inVal variable
+    var inVal = d3.select("#user-input").property("value");
+
+
+    // this capitalizes the first letter if end-user input a region with all lowercase (italy -> Italy)
+    var inputValue = inVal.charAt(0).toUpperCase() + inVal.slice(1);
+  
     
-    var inputValue = d3.select("#user-input").property("value");
-   
     
+    /*
+    This filters the results by looking at the country column. 
+   results will be stored when #user-input matches the column value we are querying (i.e., country)
+     */
     var filteredWines = wines.filter(wines => wines.country.includes(inputValue));
-    
- 
-    
-    
-    var output = _.sortBy(filteredWines, 'points').reverse()
+
+        
+
+
+
+
+    // this sorts the results by a *different column* (i.e., price)
+    var output = _.sortBy(filteredWines, 'price')
    
     
+    //finally, utilize the output variable holding the queried info (filteredWines), 
+    // loop through each desired column and add them to the table one-by-one
 
     for (var i = 0; i < filteredWines.length; i++)
         {d3.select("tbody").insert("tr").html(
-            "<td>" + [i+1] + "</td>" + 
-            "<td>" + (output[i]['country'])+"</a>"+"</td>"+
+           // "<td>" + [i+1] + "</td>" + 
+            "<td>" + (output[i]['title'])+"</td>"+
+            "<td>" + (output[i]['winery'])+"</td>"+
+            "<td>" + (output[i]['province'])+"</td>"+
             "<td>" + (output[i]['description'])+"</td>"+
-            "<td>" + (output[i]['points'])+"</td>"+
             "<td>" + (output[i]['price'])+"</td>")}
     }; 
 
 });
     
 
+
+// Source ~ 
+// J. Arias,J. (2021) Building a simple website that outputs results from a CSV using user’s input 
+// [Article] https://tinyurl.com/js-csv-reader
+
+// Honorable mention to Mr. Arias - We were struggling to import & query a csv this size
